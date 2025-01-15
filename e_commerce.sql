b0395168b855bd24e42d0d5b40eb262b217b2a88
Dovile-A <dovile.aleksandraviciute@gmail.com>
2025-01-15 16:27:12 +0100

    Update e_commerce.sql with latest schema changes

--- e_commerce.sql
+++ e_commerce.sql
@@ -64,6 +64,97 @@ UPDATE Sales s
     JOIN Products p ON s.product_id = p.id
     SET s.revenue_€ = p.price_€ * s.quantity;
 
+-- Step 5: Insights
+
+-- Total Revenue
+-- Query to calculate the total revenue from all sales
+
+SELECT SUM(revenue_€) AS total_revenue_€
+FROM Sales;
+
+-- Monthly revenue information
+-- Query to retrieve the total revenue by month and year
+
+SELECT 
+    YEAR(STR_TO_DATE(o.order_date, '%d/%m/%Y')) AS year,
+    MONTH(STR_TO_DATE(o.order_date, '%d/%m/%Y')) AS month,
+    SUM(s.revenue_€) AS total_revenue
+FROM Sales s
+JOIN Orders o ON s.order_id = o.id
+GROUP BY 
+    YEAR(STR_TO_DATE(o.order_date, '%d/%m/%Y')),
+    MONTH(STR_TO_DATE(o.order_date, '%d/%m/%Y'))
+ORDER BY 
+    year ASC, 
+    month ASC;
+    
+-- Revenue of each product
+-- Query to calculate the revenue for each product, including the quantity sold
+
+SELECT 
+    p.mobile_device_brand,
+    p.mobile_device_model,
+    p.mobile_device_os,
+    SUM(s.quantity) AS total_quantity_sold,
+    SUM(s.revenue_€) AS total_revenue_€
+FROM sales s
+JOIN products p ON s.product_id = p.id
+GROUP BY 
+    p.mobile_device_brand, 
+    p.mobile_device_model, 
+    p.mobile_device_os
+ORDER BY total_revenue_€ DESC;
+    
+-- Average revenue per order
+-- Query to calculate the average revenue per order
+
+SELECT 
+    ROUND(AVG(total_revenue), 2) AS average_revenue_per_order
+FROM (
+    SELECT 
+        order_id, 
+        SUM(revenue_€) AS total_revenue
+    FROM Sales
+    GROUP BY order_id
+) AS OrderRevenue;
+
+-- Demographic information
+-- Query to retrieve the order count by country and city of customers
+
+SELECT 
+    c.country,
+    c.city,
+    COUNT(o.id) AS order_count
+FROM Orders o
+JOIN Customers c ON o.customer_id = c.id
+GROUP BY 
+    c.country, 
+    c.city
+ORDER BY order_count DESC;
+
+-- Customers that spend the most
+-- Query to find the customers who spent the most on purchases
+
+SELECT 
+    c.first_name,
+    c.last_name,
+    c.e_mail,
+    c.address,
+    c.city,
+    c.country,
+    SUM(s.quantity) AS total_quantity_sold,
+    SUM(s.revenue_€) AS total_revenue_€
+FROM Sales s
+JOIN Orders o ON s.order_id = o.id
+JOIN Customers c ON o.customer_id = c.id
+GROUP BY 
+    c.first_name,
+    c.last_name,
+    c.e_mail,
+    c.address,
+    c.city,
+    c.country
+ORDER BY total_revenue_€ DESC;
 
 
 
